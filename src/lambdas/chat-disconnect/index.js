0e53e353f6998ce310f9022d1df658cf63599bfc
Roy Ronalds <tchalvakspam@gmail.com>
2023-04-19 20:54:00 -0400

    Add lambda source code.

---
+++ src/lambdas/chat-disconnect/index.js
@@ -0,0 +1,7 @@
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+function removeConnectionId(connectionId) { return dynamo.delete({ TableName: 'chat', Key: { connectionid: connectionId, }, }).promise(); }
+
+exports.handler = (event, context, callback) => { const connectionId = event.requestContext.connectionId; removeConnectionId(connectionId).then(() => { callback(null, { statusCode: 200, }) }); }
---
+++ src/lambdas/chat-on-message/index.js
@@ -0,0 +1,73 @@
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+//require('./patch.js');
+require('aws-sdk/lib/node_loader');
+var AWSL = require('aws-sdk/lib/core');
+var Service = AWSL.Service;
+var apiLoader = AWSL.apiLoader;
+apiLoader.services['apigatewaymanagementapi'] = {};
+AWSL.ApiGatewayManagementApi = Service.defineService('apigatewaymanagementapi', ['2018-11-29']);
+Object.defineProperty(apiLoader.services['apigatewaymanagementapi'], '2018-11-29', {
+    get: function get() {
+        var model = {
+            "metadata": {
+                "apiVersion": "2018-11-29",
+                "endpointPrefix": "execute-api",
+                "signingName": "execute-api",
+                "serviceFullName": "AmazonApiGatewayManagementApi",
+                "serviceId": "ApiGatewayManagementApi", "protocol": "rest-json", "jsonVersion": "1.1", "uid": "apigatewaymanagementapi-2018-11-29", "signatureVersion": "v4"
+            }, "operations": { "PostToConnection": { "http": { "requestUri": "/@connections/{connectionId}", "responseCode": 200 }, "input": { "type": "structure", "members": { "Data": { "type": "blob" }, "ConnectionId": { "location": "uri", "locationName": "connectionId" } }, "required": ["ConnectionId", "Data"], "payload": "Data" } } }, "shapes": {}
+        }
+        model.paginators = {
+            "pagination": {}
+        }
+        return model;
+    }, enumerable: true, configurable: true
+});
+
+
+let send = undefined;
+function init(event) {
+    console.log(event);
+    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+
+    });
+
+    send = async (connectionId, data) => {
+        await apigwManagementApi.postToConnection({
+            ConnectionId: connectionId,
+            Data: `Echo: ${data}`
+
+        }).promise();
+
+    }
+}
+
+
+function getConnections() {
+    return ddb.scan({ TableName: 'chat', }).promise();
+}
+
+exports.handler = (event, context, callback) => {
+    init(event);
+    if (!event || !event.body) {
+        throw new Error('No message available in the body')
+    }
+    const bodyContent = JSON.parse(event.body) ?? {};
+    let message = bodyContent.message;
+    console.info('Message was:', message);
+    getConnections().then((data) => {
+        console.log('data.Items was: ', data.Items);
+        data.Items.forEach(function (connection) {
+            console.log("Connection " + connection.connectionid);
+            send(connection.connectionid, message);
+        });
+    });
+    return {
+    }
+};
+
